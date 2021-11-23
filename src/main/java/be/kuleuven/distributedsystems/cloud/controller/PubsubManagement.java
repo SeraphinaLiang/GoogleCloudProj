a2a4a3302da02423ac9d057a8ee007d49de20b9e
OnionK <starkkyrie@gmail.com>
2021-11-23 02:04:30 +0100

    final

---
+++ src/main/java/be/kuleuven/distributedsystems/cloud/controller/PubsubManagement.java
@@ -0,0 +1,18 @@
+package be.kuleuven.distributedsystems.cloud.controller;/**
+ * @author ：mmzs
+ * @date ：Created in 2021/11/22 23:44
+ * @description：Pubsub Initiation and Logic
+ * @modified By：
+ * @version: $
+ */
+
+/**
+ * @author     ：mmzs
+ * @date       ：Created in 2021/11/22 23:44
+ * @description：Pubsub Initiation and Logic
+ * @modified By：
+ * @version: $
+ */
+
+public class PubsubManagement {
+}
