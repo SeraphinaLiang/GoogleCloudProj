eaf477c8f3d42c575172dcaed3e414db95b1d12f
OnionK <starkkyrie@gmail.com>
2021-12-17 00:13:23 +0100

    auth

--- src/main/java/be/kuleuven/distributedsystems/cloud/auth/AuthenticationJWT.java
+++ src/main/java/be/kuleuven/distributedsystems/cloud/auth/AuthenticationJWT.java
@@ -121,37 +121,7 @@ public class AuthenticationJWT {
 
         DecodedJWT jwt = null;
         try {
-//            String pri =
-//                    "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDLetkE0HWaNQ6j" +
-//                    "+xrku9Vdr4j2akTkU3e2Qk5XH98Mztq2qnjmSM3Aayor/E+50b7r+H5dngAf+xcm" +
-//                    "Ydjw6FD9JGpRUKNyBLaKsaIj/SHxn7E2LL9RNjHGnYzAVX291JtGlissQeoTEqmX" +
-//                    "9yb9ohfLOpaPp1Ymz2bx/PfHWKeA+vI9bxEWMgG38DZuSL6fhasUoI7YWEDGNFp5" +
-//                    "M7Y8hI8gCWiquNNFfcnZrcI6HOLoPqkMxc5LB8ImuC+NxniPRkJv4ouQABx4ICoo" +
-//                    "RGt8yvDJsSNEcM47fvkJ9VUrGTOQOC6Vbv3zQA9nVbywf5jL7xAST7NxFBfRVmqY" +
-//                    "NOTPILNBAgMBAAECggEAQUx9q4lYTDH9rksNkNzkdom36tgknp9YmgFCUbxVIK+b" +
-//                    "SaaAYOp5OOhLIs14KlowqlpW9yUWxiyWe8dHztvG7c3LXqgBo7v2dqDVtzXrODa2" +
-//                    "sHuAtAevxpzVx/1Hem1pnSfg1/WZMCC7kxyKAzgK6bm2Coi5YYAKedrkCzGco4uS" +
-//                    "P29JR9xOW0rykPSbXrCegs32SfO29UoJ8U4ToLw1Qmybz5qy02u5IJ8q7vZgPMQq" +
-//                    "5IQg/NpmcOIUa07TaGSSzpvG251dIImLILlKU+3UNeskrIFTe7X4l2drzfvePRmI" +
-//                    "w/uHz2SS9Ztc9GWlWtCLpWLp6QV+zmVj2RC0AvwHPwKBgQDpD12aKB1JiDZbG3LM" +
-//                    "7d+hrjIWcppym1nuyjde6CU8cOr4YUuHGyzfyzT+B8j1lwEIveCda22NHKwi+SFr" +
-//                    "UUCEoJMUXyi1bxNxo+vfax0rDjJLL82WHgZoAWBzqrCkMWkky0Mi7G/7WtKY6VD/" +
-//                    "eschirRj3a2PWpxtg5wH58lSxwKBgQDfgh+Aqn5fJstCNd6seRcrQIJiBqPM7QAH" +
-//                    "rCMd1h42v0kDoQGE+9eJ1kf8TYdrTd01SB8FZP40V4wdjmEPqXFGQ6U2k212+FEg" +
-//                    "anA1lTEN3KkUngmePVURlGp1SSBPI1BPD8ZH65Kif4YMVMlK5FNNReNd5B8p0eyq" +
-//                    "JtSabD5BtwKBgGBWG6jnQAPvLBjI5dFT2obojIe+45zQHoKYT/8JEqtDOb1l6mR0" +
-//                    "lT4Er2j6KtVpj+HfKwOnLmeQHI9wT6Ieuf6YqXNYSmmE+pKU4aE+k5YCjkOKBP8y" +
-//                    "dg9z0jy9p7qXOhEdfCjpdvh9eGQAvZx9UebJgPtu3JlPKS4TouGZrBxtAoGBAI38" +
-//                    "WsjhBlCqE1unXyLP9gD7BRkCwNHxCSDWoKRWnnEHCXotsQpq9lzQ7IACPHHVUB6G" +
-//                    "B3bk1nwn1ZH9HfnWGWialnzaISL/0oG2PDw936C/ugWn89I7giwdzZdechD0DRN8" +
-//                    "oOiVZVyniF+TCSDzdVvUdwDxZz6o5iCddFf5RX93AoGAA78D5AIjhypHx6yuTE7Q" +
-//                    "PLQU0KiDyNz7yL4C/1s4YBjGnR07KGReTkgT6Xbd9jdRYUtjoOsb53I3J0aLRrZG" +
-//                    "plryDvxNR/z6XfBZOVjZpCx3xub3kG95PLT85fKNCoe7MVK0rvV8aOK4LNwOzdwV" +
-//                    "VFpPThIFcwCrvULh9yXWDxc=";
-//            KeyFactory kf = KeyFactory.getInstance("RSA");
-//            EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(pri.getBytes()));
-//            privRSA = kf.generatePrivate(keySpec);
-            //privRSA = PemUtils.readPrivateKeyFromFile("src/main/resources/rsa-private.pem", "RSA");
+
             algorithmRS = Algorithm.RSA256((RSAPublicKey) pubRSA,null);
             JWTVerifier verifier = JWT.require(algorithmRS)
                     .withIssuer("auth0")
